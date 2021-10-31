0f993ad0163dd05514515a72ffd51b1784987713
mirzayogy <mirza.yogy@gmail.com>
2021-10-31 10:19:22 +0800

    Contoh Penggunaan Kotlin

--- app/src/main/java/com/mirzayogy/a211androidn/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/a211androidn/MainActivity.kt
@@ -2,10 +2,31 @@ package com.mirzayogy.a211androidn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val simpanButton = findViewById<Button>(R.id.simpanButton)
+        val batalButton = findViewById<Button>(R.id.batalButton)
+
+        val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val numberEditText = findViewById<EditText>(R.id.numberEditText)
+
+        simpanButton.setOnClickListener {
+            val nama = namaEditText.text.toString()
+            Toast.makeText(this, "Hai $nama",Toast.LENGTH_SHORT).show()
+        }
+
+        batalButton.setOnClickListener {
+            var angka = numberEditText.text.toString().trim().toInt()
+            angka ++
+            numberEditText.setText(angka.toString())
+        }
     }
 }
