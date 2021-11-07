93c999ff17d0f11cd5def3f499c6dbea8eeddc4c
mirzayogy <mirza.yogy@gmail.com>
2021-11-07 09:47:05 +0800

    menambahkan HasilActivity

---
+++ app/src/main/java/com/mirzayogy/a211androidn/HasilActivity.kt
@@ -0,0 +1,11 @@
+package com.mirzayogy.a211androidn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HasilActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil)
+    }
+}
