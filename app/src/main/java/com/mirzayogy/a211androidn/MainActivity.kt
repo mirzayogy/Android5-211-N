feca113ba1591d5e8201ed59468f8a6c743f45b3
mirzayogy <mirza.yogy@gmail.com>
2021-11-07 10:00:14 +0800

    Menggunakan Intent untuk mengirim extra

--- app/src/main/java/com/mirzayogy/a211androidn/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/a211androidn/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mirzayogy.a211androidn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -16,11 +17,28 @@ class MainActivity : AppCompatActivity() {
         val batalButton = findViewById<Button>(R.id.batalButton)
 
         val namaEditText = findViewById<EditText>(R.id.namaEditText)
+        val phoneEditText = findViewById<EditText>(R.id.phoneEditText)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val tanggalEditText = findViewById<EditText>(R.id.tanggalEditText)
         val numberEditText = findViewById<EditText>(R.id.numberEditText)
 
         simpanButton.setOnClickListener {
+
             val nama = namaEditText.text.toString()
+            val phone = phoneEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val tanggal = tanggalEditText.text.toString()
+            val number = numberEditText.text.toString()
+
             Toast.makeText(this, "Hai $nama",Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,HasilActivity::class.java)
+            intent.putExtra("nama_extra",nama)
+            intent.putExtra("phone_extra",phone)
+            intent.putExtra("email_extra",email)
+            intent.putExtra("tanggal_extra",tanggal)
+            intent.putExtra("number_extra",number)
+            startActivity(intent)
         }
 
         batalButton.setOnClickListener {
