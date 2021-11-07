dde7e20ba217abe57463379303a65ec5655a90cd
mirzayogy <mirza.yogy@gmail.com>
2021-11-07 10:06:40 +0800

    Menggunakan Intent untuk mengambil extra

--- app/src/main/java/com/mirzayogy/a211androidn/HasilActivity.kt
+++ app/src/main/java/com/mirzayogy/a211androidn/HasilActivity.kt
@@ -2,10 +2,25 @@ package com.mirzayogy.a211androidn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class HasilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hasil)
+
+        val namaTextView = findViewById<TextView>(R.id.namaTextView)
+        val phoneTextView = findViewById<TextView>(R.id.phoneTextView)
+        val emailTextView = findViewById<TextView>(R.id.emailTextView)
+        val tanggalTextView = findViewById<TextView>(R.id.tanggalTextView)
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+
+        namaTextView.setText(intent.getStringExtra("nama_extra"))
+        phoneTextView.setText(intent.getStringExtra("phone_extra"))
+        emailTextView.setText(intent.getStringExtra("email_extra"))
+        tanggalTextView.setText(intent.getStringExtra("tanggal_extra"))
+        numberTextView.setText(intent.getStringExtra("number_extra"))
+
     }
 }
